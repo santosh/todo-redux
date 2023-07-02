817035cf03f44614364e7b5576b44808f89415cd
Santosh Kumar <sntshkmr60@gmail.com>
2023-07-02 23:17:07 +0530

    Create redux store, reducer and action

---
+++ src/app/store.js
@@ -0,0 +1,39 @@
+import { createStore } from "redux"
+
+const initialState = {
+  value: [
+    { id: 1, description: "Write email", completed: false },
+    { id: 2, description: "Deep work", completed: false },
+    { id: 3, description: "Meeting", completed: false },
+    { id: 4, description: "Write design doc", completed: false },
+  ]
+}
+
+function todoReducer(state = initialState, action) {
+  switch (action.type) {
+    case "todo/ADD_TODO":
+      return {
+        ...state,
+        value: [...state.value, action.payload],
+      };
+    case "todo/DELETE_TODO":
+    // add code here
+    case "todo/CLONE_TODO":
+    // add code here
+    default:
+      return state
+  }
+}
+
+const store = createStore(todoReducer)
+
+// ACTIONS
+
+export const addTodo = (newTodo) => {
+  return {
+    type: "todo/ADD_TODO",
+    payload: newTodo
+  }
+}
+
+export default store
