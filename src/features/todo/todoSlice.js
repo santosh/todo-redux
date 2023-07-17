143ec4f04eab399535f8bbd53a3e32f6d09dcf3f
Santosh Kumar <sntshkmr60@gmail.com>
2023-07-17 14:48:05 +0530

    Add more TODO handlers in reducer

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -7,16 +7,17 @@ const initialState = [
 
 export function todoReducer(state = initialState, action) {
   switch (action.type) {
-    case "todo/TODO_ADDED":
+    case "todo/TODO_ADDED": {
       const newTodoToAdd = {
         ...action.payload,
         id: nextTodoId(state) // Assuming nextTodoId function exists and generates the next unique ID
       };
-      return {
+      return [
         ...state,
         newTodoToAdd
-      };
-    case "todo/TODO_TOGGLED":
+      ]
+    }
+    case "todo/TODO_TOGGLED": {
       return state.map(todo => {
         if (todo.id !== action.payload) {
           return todo
@@ -27,21 +28,41 @@ export function todoReducer(state = initialState, action) {
           completed: !todo.completed
         }
       })
+    }
+    case "todo/COLOR_SELECTED": {
+      const { color, todoId } = action.payload
+      return state.map((todo) => {
+        if (todo.id !== todoId) {
+          return todo
+        }
 
-    // case "todo/TODO_DELETED":
-    //   // return entire state value except the one in payload
-    //   return {
-    //     ...state,
-    //     todos: state.todos.filter((todo) => todo.id !== action.payload),
-    //   };
-    // case "todo/TODO_CLONED":
-    //   // add new item by cloing the one in payload
-    //   const clonedTodo = state.todos.find((todo) => todo.id === action.payload);
-    //   const newTodo = { ...clonedTodo, id: Date.now() };
-    //   return {
-    //     ...state,
-    //     todos: [...state.todos, newTodo],
-    //   }
+        return {
+          ...todo,
+          color,
+        }
+      })
+    }
+    case "todo/TODO_DELETED": {
+      // return entire state value except the one in payload
+      return state.filter((todo) => todo.id !== action.payload)
+    }
+    case "todo/TODO_CLONED":
+      // add new item by cloing the one in payload
+      const clonedTodo = state.find((todo) => todo.id === action.payload);
+      const newTodo = { ...clonedTodo, id: nextTodoId(state) };
+
+      return [
+        ...state,
+        newTodo
+      ]
+    case 'todo/TODO_ALL_COMPLETED': {
+      return state.map((todo) => {
+        return { ...todo, completed: true }
+      })
+    }
+    case 'todo/TODO_COMPLETED_CLEARED': {
+      return state.filter((todo) => !todo.completed)
+    }
     default:
       return state
   }
