f99149cfbe6276a3b972765688719cef1830bc35
Santosh Kumar <sntshkmr60@gmail.com>
2023-07-17 14:32:56 +0530

    Add COLOR_FILTER_CHANGED handler

--- src/features/todo/filterSlice.js
+++ src/features/todo/filterSlice.js
@@ -1,5 +1,11 @@
+export const StatusFilters = {
+  All: 'all',
+  Active: 'active',
+  Completed: 'completed'
+}
+
 const initialState = {
-  status: 'All',
+  status: StatusFilters.All,
   colors: []
 }
 
@@ -9,6 +15,33 @@ export function filterReducer(state = initialState, action) {
       return {
         ...state,
         status: action.payload
+      }
+    }
+    case "filter/COLOR_FILTER_CHANGED": {
+      let { color, changeType } = action.payload
+      const { colors } = state
+
+      switch (changeType) {
+        case 'added': {
+          if (colors.includes(color)) {
+            return state
+          }
+
+          return {
+            ...state,
+            colors: state.colors.concat(color)
+          }
+        }
+        case 'removed': {
+          return {
+            ...state,
+            colors: state.colors.filter(
+              (existingColor) => existingColor !== color
+            )
+          }
+        }
+        default:
+          return state
       }
     }
     default:
