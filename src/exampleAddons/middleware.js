2af660ea91d0694165f29044f9717d4e47571ad7
Santosh Kumar <sntshkmr60@gmail.com>
2023-07-22 19:46:24 +0530

    Add custom middlewares

--- src/exampleAddons/middleware.js
+++ src/exampleAddons/middleware.js
@@ -12,3 +12,22 @@ export const print3 = (storeAPI) => (next) => (action) => {
   console.log('3')
   return next(action)
 }
+
+export function exampleMiddleware(storeAPI) {
+  return function wrapDispatch(next) {
+    return function handleAction(action) {
+      // Do anything here: pass the action onwards with next(action),
+      // or restart the pipeline with storeAPI.dispatch(action)
+      // Can also use storeAPI.getState() here
+
+      return next(action)
+    }
+  }
+}
+
+export const loggerMiddleware = (storeAPI) => (next) => (action) => {
+  console.log('dispatching', action)
+  let result = next(action)
+  console.log('next state', storeAPI.getState())
+  return result
+}
