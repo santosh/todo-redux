42908c2d6db2c4665507e4aa8535af2b76a05a63
Santosh Kumar <sntshkmr60@gmail.com>
2023-07-27 21:12:14 +0530

    feat: dispatch actions inside homegrown handlers

--- src/features/todo/TodoListItem.jsx
+++ src/features/todo/TodoListItem.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
-
 import { ReactComponent as TimesSolid } from './times-solid.svg'
-
 import { availableColors, capitalize } from '../filter/colors'
+import { useSelector, useDispatch } from 'react-redux'
 
+const selectedTodoById = (state, todoId) => state.todos.find((todo) => todo.id === todoId)
 
-const TodoListItem = ({ todo, onColorChange, onCompletedChange, onDelete }) => {
+const TodoListItem = ({ id }) => {
+  const todo = useSelector(state => selectedTodoById(state, id))
   const { text, completed, color } = todo
 
-  const handleCompletedChanged = (e) => {
-    onCompletedChange(e.target.checked)
+  const dispatch = useDispatch()
+
+  const handleCompletedChanged = () => {
+    dispatch({ type: 'todo/TODO_TOGGLED', payload: todo.id })
   }
 
   const handleColorChanged = (e) => {
-    onColorChange(e.target.value)
+    dispatch({ type: 'todo/COLOR_SELECTED', payload: { todoId: todo.id, color: e.target.value } })
+  }
+
+  const handleDeleteClicked = () => {
+    dispatch({ type: 'todo/TODO_DELETED', payload: todo.id })
   }
 
   const colorOptions = availableColors.map((c) => (
@@ -39,7 +46,7 @@ const TodoListItem = ({ todo, onColorChange, onCompletedChange, onDelete }) => {
             <option value=""></option>
             {colorOptions}
           </select>
-          <button className="destroy" onClick={onDelete}>
+          <button className="destroy" onClick={handleDeleteClicked}>
             <TimesSolid />
           </button>
         </div>
