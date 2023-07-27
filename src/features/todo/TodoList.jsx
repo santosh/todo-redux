c65e57f6709d27e40bf7906fdec915939aa1dd31
Santosh Kumar <sntshkmr60@gmail.com>
2023-07-27 21:10:06 +0530

    fix: prevent re-rendering all TodoListItem

--- src/features/todo/TodoList.jsx
+++ src/features/todo/TodoList.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, shallowEqual } from 'react-redux'
 import TodoListItem from './TodoListItem'
 
-const TodoList = () => {
-  const todos = useSelector(state => state.todos)
+const selectedTodoIds = state => state.todos.map(todo => todo.id)
 
-  const renderedListItems = todos.map((todo) => {
-    return <TodoListItem key={todo.id} todo={todo} />
+const TodoList = () => {
+  const todoIds = useSelector(selectedTodoIds, shallowEqual)
+
+  const renderedListItems = todoIds.map((todoId) => {
+    return <TodoListItem key={todoId} id={todoId} />
   })
 
   return <ul className="todo-list">{renderedListItems}</ul>
 }
 
 export default TodoList
+
+// Why did we change from passing todos to passing todoIds?
+//  - We want to avoid passing the entire todo object to the TodoListItem component.
+// Why do we avoid passing the entire todo object to the TodoListItem component?
+//  - Passing the entire todo object to the TodoListItem component would cause the TodoListItem component to re-render whenever any property of the todo object changes.
